4fbe2ed1b6a01d03ad0e93d299cde98022e32b4e
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-05 12:32:17 +0200

    titolo snack 1

--- main.js
+++ main.js
@@ -6,10 +6,19 @@
 // Crea una funzione getPost(id) che recupera l'intero post.
 // Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
 
-// Snack 2
-// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6.
-// Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+fetch("https://dummyjson.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.title);
+  })
+  .catch((error) => {
+    console.error("errore nel recpero post", error);
+  });
 
-// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
-// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato.
-// Se il numero esce due volte di fila, stampa "Incredibile!".
+function getPostTitle(id) {
+  return fetch(`https://dummyjson.com/posts/${id}`)
+    .then((response) => response.json())
+    .then((data) => data.title);
+}
+
+getPostTitle(2).then((title) => console.log(title));
