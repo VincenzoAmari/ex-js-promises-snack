0ca3f9721560948b07af104f834bf196cbe3b842
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-05 13:00:21 +0200

    start snack 2

--- main.js
+++ main.js
@@ -40,3 +40,10 @@ function getPostAndUser(id) {
     });
 }
 getPostAndUser(1).then((post) => console.log(post));
+
+// Snack 2
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6.
+// Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato.
+// Se il numero esce due volte di fila, stampa "Incredibile!".
